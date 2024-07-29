40f8d6dae0c76388087f2ec0696253bf8199a2dc
Joilson Rodrigues <62257507+joilson-dev@users.noreply.github.com>
2024-07-28 21:19:10 -0300

    create navigation bar(NavBar)

---
+++ src/Body.js
@@ -0,0 +1,15 @@
+// import Stories from './Stories';
+// import User from './User';
+// import Post from './Post';
+// import Posts from './Posts';
+// import Suggestion from './Suggestion';
+// import Suggestions from './Suggestions';
+// import SideBar from './SideBar';
+
+export default function Body() {
+    return (
+        <>
+
+        </>
+    )
+}
