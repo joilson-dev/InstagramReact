dfd01cb50ad1b0ef5adf5fb40688145f7df1a246
Joilson Rodrigues <62257507+joilson-dev@users.noreply.github.com>
2024-07-31 20:47:03 -0300

    dynamic post

--- src/Post.js
+++ src/Post.js
@@ -1,43 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Post({ usuario, imgPost, imgCurtida, curtidoPor, curtidas }) {
+export default function Post({ usuario, imgPost, imgCurtida, curtidoPor, curtidas, toSalve }) {
+    const [liked, setLiked] = useState(false);
+    const [saved, setSaved] = useState(toSalve);
+    const [likesCount, setLikesCount] = useState(parseInt(curtidas.replace('.', '')));
+
+    const handleLikeClick = () => {
+        if (liked) {
+            setLikesCount(likesCount - 1);
+        } else {
+            setLikesCount(likesCount + 1);
+        }
+        setLiked(!liked);
+    };
+
+    const handleImageClick = () => {
+        if (!liked) {
+            setLikesCount(likesCount + 1);
+            setLiked(true);
+        }
+    };
+
+    const handleSaveClick = () => {
+        setSaved(!saved);
+    };
+
     return (
-        <>
-            <div className="post">
-                <div className="topo">
-                    <div className="usuario">
-                        <img src={usuario.img} alt={usuario.nome} />
-                        {usuario.nome}
+        <div className="post">
+            <div className="topo">
+                <div className="usuario">
+                    <img src={usuario.img} alt={usuario.nome} />
+                    {usuario.nome}
+                </div>
+                <div className="acoes">
+                    <ion-icon name="ellipsis-horizontal" role="img" className="md hydrated" aria-label="ellipsis horizontal"></ion-icon>
+                </div>
+            </div>
+
+            <div className="conteudo" onClick={handleImageClick}>
+                <img src={imgPost} alt="post content" />
+            </div>
+
+            <div className="fundo">
+                <div className="acoes">
+                    <div>
+                        <ion-icon
+                            name={liked ? "heart" : "heart-outline"}
+                            style={{ color: liked ? 'red' : 'black' }}
+                            onClick={handleLikeClick}
+                            role="img"
+                            className="md hydrated"
+                            aria-label="heart outline">
+                        </ion-icon>
+                        <ion-icon name="chatbubble-outline" role="img" className="md hydrated" aria-label="chatbubble outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline" role="img" className="md hydrated" aria-label="paper plane outline"></ion-icon>
                     </div>
-                    <div className="acoes">
-                        <ion-icon name="ellipsis-horizontal" role="img" className="md hydrated" aria-label="ellipsis horizontal"></ion-icon>
+                    <div>
+                        <ion-icon
+                            name={saved ? "bookmark" : "bookmark-outline"}
+                            onClick={handleSaveClick}
+                            role="img"
+                            className="md hydrated"
+                            aria-label="bookmark outline">
+                        </ion-icon>
                     </div>
                 </div>
 
-                <div className="conteudo">
-                    <img src={imgPost} alt="post content" />
-                </div>
-
-                <div className="fundo">
-                    <div className="acoes">
-                        <div>
-                            <ion-icon name="heart-outline" role="img" className="md hydrated" aria-label="heart outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline" role="img" className="md hydrated" aria-label="chatbubble outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline" role="img" className="md hydrated" aria-label="paper plane outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline" role="img" className="md hydrated" aria-label="bookmark outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div className="curtidas">
-                        <img src={imgCurtida} alt="curtido por" />
-                        <div className="texto">
-                            Curtido por <strong>{curtidoPor}</strong> e <strong>outras {curtidas} pessoas</strong>
-                        </div>
+                <div className="curtidas">
+                    <img src={imgCurtida} alt="curtido por" />
+                    <div className="texto">
+                        Curtido por <strong>{curtidoPor}</strong> e <strong>outras {likesCount.toLocaleString()} pessoas</strong>
                     </div>
                 </div>
             </div>
-        </>
-    )
-};
+        </div>
+    );
+}
--- src/Posts.js
+++ src/Posts.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Post from './Post';
 
 const postsData = [
@@ -6,39 +7,41 @@ const postsData = [
         imgPost: 'assets/img/gatos.jpeg',
         imgCurtida: 'assets/img/razoesparaacreditar.svg',
         curtidoPor: 'chibirdart',
-        curtidas: '199.159'
+        curtidas: '199.159',
+        toSalve: false
     },
     {
         usuario: { nome: 'barked', img: '/assets/img/barked.svg' },
         imgPost: '/assets/img/dog.svg',
         imgCurtida: '/assets/img/adorable_animals.svg',
         curtidoPor: 'adorable_animals',
-        curtidas: '99.159'
+        curtidas: '99.159',
+        toSalve: false
     },
     {
         usuario: { nome: 'meowed', img: '/assets/img/meowed.svg' },
         imgPost: '/assets/img/gato-telefone.svg',
         imgCurtida: '/assets/img/respondeai.svg',
         curtidoPor: 'respondeai',
-        curtidas: '101.523'
+        curtidas: '101.523',
+        toSalve: false
     }
 ];
 
 export default function Posts() {
     return (
-        <>
-            <div className="posts">
-                {postsData.map((post, index) => (
-                    <Post
-                        key={index}
-                        usuario={post.usuario}
-                        imgPost={post.imgPost}
-                        imgCurtida={post.imgCurtida}
-                        curtidoPor={post.curtidoPor}
-                        curtidas={post.curtidas}
-                    />
-                ))}
-            </div>
-        </>
-    )
+        <div className="posts">
+            {postsData.map((post, index) => (
+                <Post
+                    key={index}
+                    usuario={post.usuario}
+                    imgPost={post.imgPost}
+                    imgCurtida={post.imgCurtida}
+                    curtidoPor={post.curtidoPor}
+                    curtidas={post.curtidas}
+                    toSalve={post.toSalve}
+                />
+            ))}
+        </div>
+    );
 }
